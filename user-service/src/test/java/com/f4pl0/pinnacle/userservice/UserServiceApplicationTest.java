b0b35e73c69a35a1f50c0a60faced5767e7847c2
f4 <straka.filip@gmail.com>
2023-11-01 02:01:30 +0100

    fix: optimized imports

--- user-service/src/test/java/com/f4pl0/pinnacle/userservice/UserServiceApplicationTest.java
+++ user-service/src/test/java/com/f4pl0/pinnacle/userservice/UserServiceApplicationTest.java
@@ -7,14 +7,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.sql.DataSource;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)

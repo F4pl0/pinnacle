45d3510cc29fa7b650fd172583e40ed1eac2db03
f4 <straka.filip@gmail.com>
2023-11-03 03:02:34 +0100

    add: data-aggregator-service

---
+++ data-aggregator-service/src/main/java/com/f4pl0/pinnacle/dataaggregatorservice/DataAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.f4pl0.pinnacle.dataaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAggregatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataAggregatorServiceApplication.class, args);
+    }
+
+}
---
+++ data-aggregator-service/src/test/java/com/f4pl0/pinnacle/dataaggregatorservice/DataAggregatorServiceApplicationTests.java
@@ -0,0 +1,34 @@
+package com.f4pl0.pinnacle.dataaggregatorservice;
+
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.sql.DataSource;
+
+@ExtendWith(SpringExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes = H2DatabaseTestConfig.class)
+@DataJpaTest()
+class DataAggregatorServiceApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    void injectedComponentsAreNotNull() {
+        assert dataSource != null;
+        assert jdbcTemplate != null;
+        assert entityManager != null;
+    }
+}

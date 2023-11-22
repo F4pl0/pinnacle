c26a47577114565a3b2da4d2af108b516e01bc8e
f4 <straka.filip@gmail.com>
2023-11-22 12:58:54 +0100

    Improved validation messages for AddStockAssetDto fields.
    The changes in the code include refined validation messages for the symbol, quantity, and price fields in the AddStockAssetDto class. This is done to make error messages more descriptive and helpful to the users. Adding these custom messages will improve user experience by providing clear and specific validation errors.

--- portfolio-service/src/main/java/com/f4pl0/pinnacle/portfolioservice/dto/AddStockAssetDto.java
+++ portfolio-service/src/main/java/com/f4pl0/pinnacle/portfolioservice/dto/AddStockAssetDto.java
@@ -9,16 +9,17 @@ import java.math.BigDecimal;
 
 @Data
 public class AddStockAssetDto {
-    @Pattern(regexp = "^[A-Z]{1,5}$")
+    @Pattern(regexp = "^[A-Z]{1,5}$", message = "Symbol must be 1 to 5 uppercase letters")
     private String symbol;
 
-    @NotNull
-    @Min(0)
+    @NotNull(message = "Quantity must be provided")
+    @Min(value = 1, message = "Quantity must be at least 1")
     private long quantity;
 
-    @NotNull
-    @Min(0)
+    @NotNull(message = "Price must be provided")
+    @Min(value = 0, message = "Price must be at least 0")
     private BigDecimal price;
 
+    // No validation for purchaseTimestamp as it's optional
     private long purchaseTimestamp;
 }

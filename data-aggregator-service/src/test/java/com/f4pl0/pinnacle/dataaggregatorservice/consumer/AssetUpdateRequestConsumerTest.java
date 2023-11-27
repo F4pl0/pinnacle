fbeb90d7a29f3ada0b9c37b79cb78eef0b85c531
f4 <straka.filip@gmail.com>
2023-11-27 04:31:15 +0100

    Add tests for AssetUpdateRequestConsumer and update properties
    Created a new test class, AssetUpdateRequestConsumerTest, to ensure proper functioning of the AssetUpdateRequestConsumer. The test verifies that the consumer handles the Kafka events without causing any exceptions. Also, the 'application-test.properties' file has been updated to include the Kafka bootstrap server configuration, necessary for the test environment.

---
+++ data-aggregator-service/src/test/java/com/f4pl0/pinnacle/dataaggregatorservice/consumer/AssetUpdateRequestConsumerTest.java
@@ -0,0 +1,39 @@
+package com.f4pl0.pinnacle.dataaggregatorservice.consumer;
+
+import com.f4pl0.pinnacle.dataaggregatorservice.H2DatabaseTestConfig;
+import com.f4pl0.pinnacle.dataaggregatorservice.event.AssetUpdateRequestEvent;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, brokerProperties = {"listeners=PLAINTEXT://localhost:19092", "port=19092"})
+@ContextConfiguration(classes = {H2DatabaseTestConfig.class})
+public class AssetUpdateRequestConsumerTest {
+    @Autowired
+    private KafkaTemplate<String, AssetUpdateRequestEvent> kafkaTemplate;
+
+    @Mock
+    private AssetUpdateRequestConsumer consumer;
+
+    @Test
+    public void testPublishAndListen() {
+        AssetUpdateRequestEvent event = new AssetUpdateRequestEvent("AAPL", AssetUpdateRequestEvent.AssetType.STOCK);
+        kafkaTemplate.send("asset-update-request-topic", event);
+
+        // Verify that the consumer doesn't throw any exceptions
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

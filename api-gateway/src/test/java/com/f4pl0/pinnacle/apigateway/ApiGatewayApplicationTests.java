e109fb7e0ab94f6f9c23d4793920c7719265311d
f4 <straka.filip@gmail.com>
2023-12-02 09:51:21 +0100

    Add additional testing to ApiGatewayApplicationTests
    Added two unit tests in the ApiGatewayApplicationTests class to verify that the application context is correctly initialised and that the main method properly starts up the Application. These tests were included to enhance coverage and provide further assurance of the Application's correct behaviour.

--- api-gateway/src/test/java/com/f4pl0/pinnacle/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/f4pl0/pinnacle/apigateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,24 @@
 package com.f4pl0.pinnacle.apigateway;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void mainStartsApplication() {
+        ApiGatewayApplication.main(new String[] {});
+        assertThat(applicationContext).isNotNull();
+    }
 }

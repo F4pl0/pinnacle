eeb53fd8af82c068c4f3eb2532d383a2a3eff706
f4 <straka.filip@gmail.com>
2023-12-02 09:49:57 +0100

    Add extra tests to DiscoveryServerApplicationTests
    Included an application context validator and a main method invocation test to the DiscoveryServerApplicationTests class. The context validator asserts that the application context is successfully initialized upon starting the application. The main method invocation test confirms that the main method can correctly start the application. These added tests increase the coverage and certainty of functionality for the Discovery Server application.

--- discovery-server/src/test/java/com/f4pl0/pinnacle/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/f4pl0/pinnacle/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.f4pl0.pinnacle.discoveryserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DiscoveryServerApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void mainStartsApplication() {
+        DiscoveryServerApplication.main(new String[] {});
+        assertThat(applicationContext).isNotNull();
+    }
 }
